4ed484b75a46a58c038d4a418e5191cfb71063ed
shilvlad <a@iteko.su>
2019-01-06 15:39:10 +0300

    1

--- portal/urls.py
+++ portal/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     url(r'^$', views.start, name='start'),
 #    url(r'^contacts/', views.contacts, name='contacts'),
 #    url(r'^userrequest/', views.userrequest, name='userrequest'),
-#    url(r'^login/', views.user_login, name='login'),
-#    url(r'^logout/', views.user_logout, name='logout'),
+    url(r'^login/', views.user_login, name='login'),
+    url(r'^logout/', views.user_logout, name='logout'),
 #    url(r'^profile/', views.profile, name='profile'),
 
 ]
--- portal/views.py
+++ portal/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def start(request):
     return render(request, 'portal/index.html', {})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print ("Invalid login details:")
+            return HttpResponse("Invalid login details supplied.")
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'portal/index.html',{})
