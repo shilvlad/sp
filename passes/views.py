81fffe6cca9b8049eedd8ee3cf0a8cb0e04a5fc5
ilya <a@iteko.su>
2020-12-07 12:51:53 +0300

    1.3

--- passes/views.py
+++ passes/views.py
@@ -75,7 +75,36 @@ def start(request, pass_id=None):
 
 
     if context['role'] == 'controller':
-        pass
+
+
+        allPasses = Passes.objects.all().filter(deleted=False)
+
+        warning_days = 120
+        danger_days = 90
+        critical_days = 30
+
+        for i in allPasses:
+            print(i.author, request.user)
+            if i.author == request.user:
+                i.showmy = True
+            else:
+                i.showmy = False
+
+            i.days_left = int(str(i.passexpired - datetime.date.today()).split()[0])
+
+            if i.days_left > warning_days:
+                i.status = "table-success"
+
+            elif i.days_left <= warning_days and i.days_left > danger_days + 1:
+                i.status = "table-warning"
+
+            elif i.days_left <= danger_days and i.days_left > critical_days + 1:
+                i.status = "table-danger"
+
+            else:
+                i.status = "table-dark"
+
+        context['passes'] = allPasses
 
     if context['role'] == 'admin':
         pass
@@ -101,7 +130,7 @@ def check_passes(request):
     danger_days = 90
     critical_days = 30
 
-    passes = Passes.objects.all().filter(deleted=False)
+    passes = Passes.objects.all().filter(deleted=False).filter(passtype='mash')
     warning_passes = []
     msg = ''
     for i in passes:
